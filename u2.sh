a4cf74436a08361c3865898b05ac666c6884c548
benaryorg <binary@benary.org>
2015-02-09 14:13:48 +0100

    added u2

--- u2.sh
+++ u2.sh
@@ -1,2 +1,23 @@
 #!/bin/sh
 
+source ./functions.sh
+
+if [ $# -ne 2 ];then
+	exit 1
+fi
+
+isint $1
+if [ $? -eq 1 ];then
+	exit 2
+fi
+
+isint $2
+if [ $? -eq 1 ];then
+	exit 2
+fi
+
+if [ $1 -gt $2 ];then
+	exit 3
+fi
+
+eval echo {$1..$2}
